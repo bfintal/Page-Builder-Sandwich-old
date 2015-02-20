89b8f62fedf1beabddb1a49dcf853fd01012c28d
Herman <oherman@gmail.com>
2015-02-20 14:27:34 +0800

    Revert "UPDATE: Google Maps now render properly but cannot adapt existing google maps shortcodes. Traced to an issue to PB Sandwich itself."
    This reverts commit 924fd241b5d231f1bfef06451473c6b1b23dc455.

--- lib/shortcode/jetpack-googlemaps.php
+++ lib/shortcode/jetpack-googlemaps.php
@@ -16,42 +16,17 @@ function sandwich_jetpack_googlemaps() {
 	shortcode_ui_register_for_shortcode(
         'googlemaps',
         array(
-            'label' => __( 'Jetpack' , 'jetpack' ) . ' ' . _x( 'Google Maps', 'Module Name', 'jetpack' ),
-            'listItemImage' => 'dashicons-location-alt',
+            'label' => __( 'Jetpack' , 'jetpack' ) . ' ' . _x( 'Google Maps Shortcode', 'Module Name', 'jetpack' ),
+            'listItemImage' => 'dashicons-email-alt',
             'attrs' => array(
-                //array(
-                //    'label' => '',
-                //    'attr'  => 'content',
-                //    'type'  => 'textarea',
-                //),
+                array(
+                    'label' => '',
+                    'attr'  => 'content',
+                    'type'  => 'textarea',
+                ),
 			),
         )
     );
-
-    add_shortcode( 'sandwich-googlemaps', 'sandwich_googlemaps' ); 
-
-    shortcode_ui_register_for_shortcode(
-        'sandwich-googlemaps',
-        array(
-            'label' => 'Jetpack Google Maps',
-            'listItemImage' => 'dashicons-location-alt',
-            'attrs' => array(
-                array(
-                    'label' => 'IFrame embed code',
-                    'attr' => 'content',
-                    'type' => 'textarea',
-					'description' => 'Enter embed code generated from Google Maps.',
-                ),
-                array(
-                    'label' => 'Height',
-                    'attr' => 'height',
-                    'type' => 'text',
-					'description' => 'Enter height in pixels',
-					'value' => '300'
-                ),
-            ),
-        )
-    );	
 	
 	// Make sure Jetpack is activated
 	if ( ! class_exists( 'Jetpack' ) ) {
@@ -60,41 +35,15 @@ function sandwich_jetpack_googlemaps() {
 	}
 
 	// Make sure the google maps shortcode module is turned on
-	if ( ! Jetpack::is_module_active( 'shortcodes' ) ) {
-		add_action( 'print_media_templates', 'sandwich_jetpack_shortcodes_googlemaps_disabled' );		
+	if ( ! Jetpack::is_module_active( 'googlemaps' ) ) {
+		add_action( 'print_media_templates', 'sandwich_jetpack_googlemaps_disabled' );
 		return;
 	}
 	
+	
 }
 
-function sandwich_googlemaps( $attr, $content = '' ) {
-    $attr = wp_parse_args( $attr, array(
-        'height' => '300'
-    ) );
-
-	preg_match( "/\<iframe.+src\=(?:\"|\')(.+?)(?:\"|\')(?:.+?)\>/", $content, $matches );
-
-	if ( count( $matches ) < 1 ) {
-		return '';
-	}
-
-	$filteredcontent = $matches[1] . "&amp;w=100%&amp;h=" . $attr['height'];
-
-    ob_start();
-    ?>
-
-    <div<?php echo is_admin() ? ' style="pointer-events: none' : '' ?>">
-	   		<?php echo do_shortcode( '[googlemaps ' . $filteredcontent . ']' ) ?>
-    </div>
-
-    <?php
-    return ob_get_clean();
-}
 
 function sandwich_jetpack_googlemaps_disabled() {
-	GambitPBSandwich::printDisabledShortcakeStlyes( 'sandwich-googlemaps', "Requires Jetpack plugin" );
-}
-
-function sandwich_jetpack_shortcodes_googlemaps_disabled() {
-	GambitPBSandwich::printDisabledShortcakeStlyes( 'sandwich-googlemaps', "Requires Jetpack's shortcode embed module" );
+	GambitPBSandwich::printDisabledShortcakeStlyes( 'googlemaps', "Requires Jetpack's shortcode module" );
 }
