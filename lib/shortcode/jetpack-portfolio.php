1f6aada5c58b05eb912dab34b7dbd1d5b66f010d
Herman <oherman@gmail.com>
2015-02-24 00:12:29 +0800

    Applied requested changes such as spacings, change of parameters and more.

--- lib/shortcode/jetpack-portfolio.php
+++ lib/shortcode/jetpack-portfolio.php
@@ -1,21 +1,21 @@
 <?php
 
-$numberOfColumns = array();
-for ( $i = 1; $i <= 6; $i++ ) {
-	$numberOfColumns[ $i ] = $i;
-}
-
-$displayOrder['author'] = __( 'Author', 'pbsandwich' );
-$displayOrder['date'] = __( 'Post Date', 'pbsandwich' );
-$displayOrder['title'] = __( 'Post Title', 'pbsandwich' );
-$displayOrder['rand'] = __( 'Randomized', 'pbsandwich' );
-
-$displayDir['ASC'] = __( 'Ascending', 'pbsandwich' );
-$displayDir['DESC'] = __( 'Descending', 'pbsandwich' );
-
 add_action( 'init', 'sandwich_jetpack_portfolio', 11 );
 
 function sandwich_jetpack_portfolio() {
+
+	$numberOfColumns = array();
+	for ( $i = 1; $i <= 6; $i++ ) {
+		$numberOfColumns[ $i ] = $i;
+	}
+
+	$displayOrder['author'] = __( 'Author', 'pbsandwich' );
+	$displayOrder['date'] = __( 'Post Date', 'pbsandwich' );
+	$displayOrder['title'] = __( 'Post Title', 'pbsandwich' );
+	$displayOrder['rand'] = __( 'Randomized', 'pbsandwich' );
+
+	$displayDir['ASC'] = __( 'Ascending', 'pbsandwich' );
+	$displayDir['DESC'] = __( 'Descending', 'pbsandwich' );
 
 	// Check if Shortcake exists
 	if ( ! function_exists( 'shortcode_ui_register_for_shortcode') ) {
@@ -30,71 +30,65 @@ function sandwich_jetpack_portfolio() {
             'attrs' => array(
                 array(
                     'label' => __( 'Display Portfolio Types', 'pbsandwich' ),
-                    'attr'  => 'display_types',
-                    'type'  => 'checkbox',
+                    'attr' => 'display_types',
+                    'type' => 'checkbox',
                 ),
                 array(
                     'label' => __( 'Display Portfolio Tags', 'pbsandwich' ),
-                    'attr'  => 'display_tags',
-                    'type'  => 'checkbox',
+                    'attr' => 'display_tags',
+                    'type' => 'checkbox',
                 ),
                 array(
                     'label' => __( 'Display Portfolio Content', 'pbsandwich' ),
-                    'attr'  => 'display_content',
-                    'type'  => 'checkbox',
+                    'attr' => 'display_content',
+                    'type' => 'checkbox',
                 ),
                 array(
                     'label' => __( 'Entry Types to display', 'pbsandwich' ),
-                    'attr'  => 'include_type',
-                    'type'  => 'text',
+                    'attr' => 'include_type',
+                    'type' => 'text',
 					'description' => __( 'Display Portfolio Types', 'pbsandwich' )'Enter slug names of entries to display. If none entered, defaults to displaying all entries',
                 ),
                 array(
                     'label' => __( 'Tags to display', 'pbsandwich' ),
-                    'attr'  => 'include_tag',
-                    'type'  => 'text',
+                    'attr' => 'include_tag',
+                    'type' => 'text',
 					'description' => __( 'Enter slug names of tags to display. If none entered, defaults to displaying all tags', 'pbsandwich' ),
                 ),
                 array(
                     'label' => __( 'Columns to display', 'pbsandwich' ),
-                    'attr'  => 'columns',
-                    'type'  => 'select',
+                    'attr' => 'columns',
+                    'type' => 'select',
 					'description' => __( 'Enter slug names of entries to display. If none entered, defaults to displaying all entries', 'pbsandwich' ),
 					'options' => $numberOfColumns,
-					'default' => '2',
+					'value' => '2',
                 ),
                 array(
                     'label' => __( 'Amount of posts to display', 'pbsandwich' ),
-                    'attr'  => 'showposts',
-                    'type'  => 'text',
+                    'attr' => 'showposts',
+                    'type' => 'text',
 					'description' => __( 'Enter the number of entries to display. If none entered, defaults to 5', 'pbsandwich' ),
 					'value' => '5',
                 ),
                 array(
                     'label' => __( 'Display Order', 'pbsandwich' ),
-                    'attr'  => 'order',
-                    'type'  => 'select',
+                    'attr' => 'order',
+                    'type' => 'select',
 					'description' => __( 'Choose sorting direction. Ascending takes your latest post to the last page, while Descending displays your latest post first.', 'pbsandwich' ),
 					'options' => $displayDir,
-					'default' => 'DESC',
+					'value' => 'DESC',
                 ),
                 array(
                     'label' => __( 'Display Order criteria', 'pbsandwich' ),
-                    'attr'  => 'orderby',
-                    'type'  => 'select',
+                    'attr' => 'orderby',
+                    'type' => 'select',
 					'description' => __( 'Choose the criteria of entries to be sorted by.', 'pbsandwich' ),
 					'options' => $displayOrder,
-					'default' => 'date',
+					'value' => 'date',
                 ),
 			),
         )
     );
-	
-	// Make sure Jetpack is activated
-	if ( ! class_exists( 'Jetpack' ) ) {
-		add_action( 'print_media_templates', 'sandwich_jetpack_disabled' );
-		return;
-	}
 
 	// Make sure the contact form module is turned on
 	if ( ! Jetpack::is_module_active( 'custom-content-types' ) ) {
@@ -102,10 +96,6 @@ function sandwich_jetpack_portfolio() {
 		return;
 	}
 	
-}
-
-function sandwich_jetpack_disabled() {
-	GambitPBSandwich::printDisabledShortcakeStlyes( 'portfolio', "Requires Jetpack plugin and its Custom Content Type module" );
 }
 
 function sandwich_jetpack_portfolio_disabled() {
