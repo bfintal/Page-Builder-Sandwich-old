c6211b6d0e4ed05eae014c142e3851aba0849860
Benjamin Intal <bfintal@gmail.com>
2015-02-23 18:46:24 +0800

    meta, search & tag cloud widgets

--- lib/shortcode/widget-search.php
+++ lib/shortcode/widget-search.php
@@ -4,11 +4,11 @@
  * Creates the view for the archives widget
  */
 
-add_action( 'init', 'sandwich_meta_widget', 11 );
+add_action( 'init', 'sandwich_search_widget', 11 );
 
-function sandwich_meta_widget() {
+function sandwich_search_widget() {
 	
-	add_shortcode( 'pbs_meta_widget', 'sandwich_meta_widget_shortcode' );
+	add_shortcode( 'pbs_search_widget', 'sandwich_search_widget_shortcode' );
 
 	// Check if Shortcake exists
 	if ( ! function_exists( 'shortcode_ui_register_for_shortcode') ) {
@@ -16,16 +16,16 @@ function sandwich_meta_widget() {
 	}
 	
 	shortcode_ui_register_for_shortcode(
-        'pbs_meta_widget',
+        'pbs_search_widget',
         array(
-            'label' => __( 'Widget - Meta', 'pbsandwich' ),
+            'label' => __( 'Widget - Search', 'pbsandwich' ),
             'listItemImage' => 'dashicons-wordpress',
             'attrs' => array(
                 array(
                     'label' => __( 'Title', 'pbsandwich' ),
                     'attr' => 'title',
                     'type' => 'text',
-					'value' => __( 'Meta', 'pbsandwich' ),
+					'value' => __( 'Search', 'pbsandwich' ),
 					'description' => __( 'The title to display for this widget', 'pbsandwich' ),
                 ),
 				array(
@@ -40,10 +40,10 @@ function sandwich_meta_widget() {
 	
 }
 
-function sandwich_meta_widget_shortcode( $attr, $content ) {
+function sandwich_search_widget_shortcode( $attr, $content ) {
 		
 	$attr = wp_parse_args( $attr, array(
-        'title' => __( 'Meta', 'pbsandwich' ),
+        'title' => __( 'Search', 'pbsandwich' ),
 		'hide_title' => false
     ) );
 	
@@ -56,7 +56,7 @@ function sandwich_meta_widget_shortcode( $attr, $content ) {
 	
 	?>
 	<div class="sandwich <?php echo $hideTitleClass ?>">
-		<?php the_widget( 'WP_Widget_Meta', $attr ); ?>
+		<?php the_widget( 'WP_Widget_Search', $attr ); ?>
 	</div>
 	
 	<?php
---
+++ lib/shortcode/widget-tag-cloud.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * Creates the view for the archives widget
+ */
+
+add_action( 'init', 'sandwich_tag_cloud_widget', 11 );
+
+function sandwich_tag_cloud_widget() {
+	
+	add_shortcode( 'pbs_tag_cloud_widget', 'sandwich_tag_cloud_widget_shortcode' );
+
+	// Check if Shortcake exists
+	if ( ! function_exists( 'shortcode_ui_register_for_shortcode') ) {
+		return;
+	}
+	
+	$options = array();
+	foreach ( get_taxonomies() as $taxonomy ) {
+		$tax = get_taxonomy( $taxonomy );
+		if ( ! $tax->show_tagcloud || empty( $tax->labels->name ) ) {
+			continue;
+		}
+		$options[ esc_attr( $taxonomy ) ] = esc_attr( $tax->labels->name );
+	}
+	
+	shortcode_ui_register_for_shortcode(
+        'pbs_tag_cloud_widget',
+        array(
+            'label' => __( 'Widget - Tag Cloud', 'pbsandwich' ),
+            'listItemImage' => 'dashicons-wordpress',
+            'attrs' => array(
+                array(
+                    'label' => __( 'Title', 'pbsandwich' ),
+                    'attr' => 'title',
+                    'type' => 'text',
+					'value' => __( 'Tag Cloud', 'pbsandwich' ),
+					'description' => __( 'The title to display for this widget', 'pbsandwich' ),
+                ),
+                array(
+					'label' => __( 'Taxonomy', 'pbsandwich' ),
+                    'attr' => 'taxonomy',
+                    'type' => 'select',
+					'value' => '',
+					'options' => $options,
+                ),
+			),
+        )
+    );
+	
+}
+
+function sandwich_tag_cloud_widget_shortcode( $attr, $content ) {
+	
+	$default = '';
+	foreach ( get_taxonomies() as $taxonomy ) {
+		$tax = get_taxonomy( $taxonomy );
+		if ( ! $tax->show_tagcloud || empty( $tax->labels->name ) ) {
+			continue;
+		}
+		$default = esc_attr( $taxonomy );
+		break;
+	}
+		
+	$attr = wp_parse_args( $attr, array(
+        'title' => __( 'Tag Cloud', 'pbsandwich' ),
+		'taxonomy' => $default,
+		'hide_title' => false
+    ) );
+	
+	$hideTitleClass = '';
+	if ( $attr['hide_title'] ) {
+		$hideTitleClass = 'hide_title';
+	}
+			
+	ob_start();
+	
+	?>
+	<div class="sandwich <?php echo $hideTitleClass ?>">
+		<?php the_widget( 'WP_Widget_Tag_Cloud', $attr ); ?>
+	</div>
+	
+	<?php
+		
+	return ob_get_clean();
+}
