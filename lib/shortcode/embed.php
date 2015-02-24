8f98580132acff7aac1454f27c44ce50282ecee6
Benjamin Intal <bfintal@gmail.com>
2015-02-24 21:03:14 +0800

    Removed stuff which are not needed

--- lib/shortcode/embed.php
+++ lib/shortcode/embed.php
@@ -12,33 +12,17 @@ function sandwich_embed_media() {
 	shortcode_ui_register_for_shortcode(
 		'embed',
 		array(
-			'label' => __( 'Jetpack' , 'jetpack' ) . ' ' . _x( 'Embed Video Media', 'Module Name', 'jetpack' ),
-			'listItemImage' => 'dashicons-portfolio',
+			'label' => __( 'Jetpack' , 'jetpack' ) . ' ' . __( ' Embed Media', 'pbsandwich' ),
+			'listItemImage' => 'dashicons-admin-media',
 			'attrs' => array(
 				array(
 					'label' => __( 'Media URL', 'pbsandwich' ),
 					'attr' => 'content',
 					'type' => 'text',
-					'description' => __( 'Enter the URL of the media you wish to embed. Supported sites include Animoto, Blip, CollegeHumor, DailyMotion, Flickr, FunnyOrDie, Hulu, Imgur, Instagram, iSnare, Issuu, Meetup, EmbedArticles, Mixcloud, Photobucket, PollDaddy, Rdio, Revision3, Scribd, SlideShare, SmugMug, SoundCloud, Spotify, TED, Vimeo, Vine, WordPress.tv and YouTube. If you have Jetpack\'s Shortcode Embeds module enabled, you can also embed Facebook, Github Gist, Google+, and Medium links', 'pbsandwich' ),
+					'description' => __( "Enter the URL of the media you wish to embed. Supported sites include Animoto, Blip, CollegeHumor, DailyMotion, Flickr, FunnyOrDie, Hulu, Imgur, Instagram, iSnare, Issuu, Meetup, EmbedArticles, Mixcloud, Photobucket, PollDaddy, Rdio, Revision3, Scribd, SlideShare, SmugMug, SoundCloud, Spotify, TED, Vimeo, Vine, WordPress.tv and YouTube. If you have Jetpack's Shortcode Embeds module enabled, you can also embed Facebook, Github Gist, Google+, and Medium links", 'pbsandwich' ),
 				),
 			),
 		)
 	);
 	
-	// Make sure Jetpack is activated
-	if ( ! class_exists( 'Jetpack' ) ) {
-		add_action( 'print_media_templates', 'sandwich_embed_media_disabled' );
-		return;
-	}
-
-	// Make sure the contact form module is turned on
-	if ( ! Jetpack::is_module_active( 'custom-content-types' ) ) {
-		add_action( 'print_media_templates', 'sandwich_embed_media_disabled' );
-		return;
-	}
-	
-}
-
-function sandwich_embed_media_disabled() {
-	GambitPBSandwich::printDisabledShortcakeStlyes( 'portfolio', __( "Requires Jetpack's Custom Content Type module", 'pbsandwich' ) );
 }
