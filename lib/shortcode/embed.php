1429629a0e0eca16506a64fa1e48e9e7db849c6a
Benjamin Intal <bfintal@gmail.com>
2015-02-25 18:13:51 +0800

    Fix to allow the embed shortcode to run in the preview

--- lib/shortcode/embed.php
+++ lib/shortcode/embed.php
@@ -26,3 +26,22 @@ function sandwich_embed_media() {
 	);
 	
 }
+
+
+/**
+ * Inside the admin, the embed shortcode doesn't work like normal shortcodes.
+ * At the start it is unset, then gets added in to parse the post content.
+ * This fixes this by adding the embed shortcode before the preview is rendered.
+ */
+add_action( 'shortcode_ui_before_do_shortcode', 'embed_fix_shortcode_ui_before_do_shortcode' );
+function embed_fix_shortcode_ui_before_do_shortcode( $shortcode ) {
+	if ( ! preg_match( '/\[embed\s/', $shortcode ) ) {
+		return;
+	}
+	if ( ! is_admin() ) {
+		return;
+	}
+	
+	$wpEmbed = new WP_Embed();
+	add_shortcode( 'embed', array( $wpEmbed, 'shortcode' ) );
+}
