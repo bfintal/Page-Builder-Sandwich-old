a4305c15e2f034c7033aefbe3970a6aa847f1b50
Benjamin Intal <bfintal@gmail.com>
2015-02-26 08:25:36 +0800

    Split files in editor.js

---
+++ js/editor.js
@@ -0,0 +1,5 @@
+// @codekit-append "_editor-start.js";
+// @codekit-append "_editor-core.js";
+// @codekit-append "_editor-columns.js";
+// @codekit-append "_editor-jetpack.js";
+// @codekit-append "_editor-end.js";
