4cdee57769d8d85d5f2a6d8a42049b3e1b2200d1
Benjamin Intal <bfintal@gmail.com>
2015-02-26 21:54:01 +0800

    Removed comment

--- js/_editor-add-post-element.js
+++ js/_editor-add-post-element.js
@@ -4,7 +4,6 @@
 jQuery(document).ready(function($) {
 	$('body').on('click', '.sandwich-add-shortcode', function() {
 		$(this).siblings('[id="insert-media-button"]').click();
-		//wp.media.frame.setState('shortcode-ui');
 		$('.media-menu .media-menu-item:contains("' + shortcodeUIData.strings.media_frame_menu_insert_label + '")').click();
 		return false;
 	});
