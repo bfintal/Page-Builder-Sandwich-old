ee7847a8c1580f2e1f7b5e7a8db9174bc28d46b4
Herman <oherman@gmail.com>
2015-02-26 18:03:38 +0800

    Enhancements for woocommerce as well as the templates to speed up development time.

--- lib/shortcode/template-jetpack.php
+++ lib/shortcode/template-jetpack.php
@@ -20,30 +20,30 @@ function sandwich_myshortcode() {
 	
 	// Register Shortcake UI
 	shortcode_ui_register_for_shortcode(
-        'myshortcode',
-        array(
-            'label' => __( 'Some Existing Shortcode', 'pbsandwich' ),
-            'listItemImage' => 'dashicons-wordpress',
-            'attrs' => array(
-                array(
-                    'label' => __( 'Content', 'pbsandwich' ),
-                    'attr'  => 'content',
-                    'type'  => 'textarea',
-                ),
-                array(
-                    'label' => __( 'Some Text', 'pbsandwich' ),
-                    'attr'  => 'some_text',
-                    'type'  => 'text',
-                ),
-                array(
-                    'label' => __( 'Some Color', 'pbsandwich' ),
-                    'attr'  => 'some_color',
-                    'type'  => 'color',
+		'myshortcode',
+		array(
+			'label' => __( 'Some Existing Shortcode', 'pbsandwich' ),
+			'listItemImage' => 'dashicons-wordpress',
+			'attrs' => array(
+				array(
+					'label' => __( 'Content', 'pbsandwich' ),
+					'attr' => 'content',
+					'type' => 'textarea',
+				),
+				array(
+					'label' => __( 'Some Text', 'pbsandwich' ),
+					'attr' => 'some_text',
+					'type' => 'text',
+				),
+				array(
+					'label' => __( 'Some Color', 'pbsandwich' ),
+					'attr' => 'some_color',
+					'type' => 'color',
 					'value' => '#333333',
-                ),
+				),
 			),
-        )
-    );
+		)
+	);
 	
 	// Make sure Jetpack is activated
 	if ( ! class_exists( 'Jetpack' ) ) {
