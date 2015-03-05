0ed6c4d0851f17febfedd3c7295d9ced1f871a87
Benjamin Intal <bfintal@gmail.com>
2015-03-05 14:55:36 +0800

    Added ninja forms shortcode

---
+++ lib/shortcode/ninja-forms.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Ninja Forms Shortcode
+ */
+
+
+/**
+ * Create our shortcode
+ */
+add_action( 'init', 'sandwich_ninjaforms', 11 );
+function sandwich_ninjaforms() {
+
+	// Check if Shortcake exists
+	if ( ! function_exists( 'shortcode_ui_register_for_shortcode' ) ) {
+		return;
+	}
+	
+	if ( ! is_admin() ) {
+		return;
+	}
+	
+	if ( ! function_exists( 'ninja_forms_get_all_forms' ) ) {
+		return;
+	}
+	
+	// Get all the Ninja Forms
+	$options = array(
+		'0' => sprintf( '— %s —', __( 'Select', 'pbsandwich' ) ),
+	);
+	
+	// @see http://docs.ninjaforms.com/article/159-ninja-formsformsget-all
+	$allForms = ninja_forms_get_all_forms();
+	
+	foreach( $allForms as $form ) {
+		if ( ! empty( $form['id'] ) && ! empty( $form['data']['form_title'] ) ) {
+			$options[ $form['id'] ] = $form['data']['form_title'];
+		}
+	}
+	
+	// Register Shortcake UI
+	shortcode_ui_register_for_shortcode(
+		'ninja_forms',
+		array(
+			'label' => __( 'Ninja Forms', 'pbsandwich' ),
+			'listItemImage' => 'dashicons-feedback',
+			'attrs' => array(
+				array(
+					'label' => __( 'Choose the form to display', 'pbsandwich' ),
+					'attr' => 'id',
+					'type' => 'select',
+					'options' => $options,
+				),
+			),
+		)
+	);
+	
+}
