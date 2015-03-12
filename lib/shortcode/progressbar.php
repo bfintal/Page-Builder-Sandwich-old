c4be7b0c2ad3e24d8e2b079148ff4a1c10925132
Herman <oherman@gmail.com>
2015-03-12 13:43:41 +0800

    Completion of features, ready for implemetation.

--- lib/shortcode/progressbar.php
+++ lib/shortcode/progressbar.php
@@ -1,6 +1,19 @@
 <?php
 
 /**
+ * Selections for button styles
+ */
+
+function sandwich_progressbar_style_selection() {
+	$output = array();
+	$output['progress-bar-info'] = "Informational";
+	$output['progress-bar-success'] = "Success";
+	$output['progress-bar-warning'] = "Warning";
+	$output['progress-bar-danger'] = "Danger";
+	return $output;
+}
+
+/**
  * Creates the view for Progress bar
  */
 
@@ -26,6 +39,27 @@ function sandwich_progressbar() {
             'listItemImage' => 'dashicons-plus',
             'attrs' => array(
                 array(
+                    'label' => __( 'Progress Bar Design', 'pbsandwich' ),
+                    'attr' => 'type',
+                    'type' => 'select',
+					'options' => sandwich_progressbar_style_selection(),
+					'description' => __( 'Choose the type of Progress Bar to use.', 'pbsandwich' ),
+                ),
+                array(
+                    'label' => __( 'Striped Progress Bar', 'pbsandwich' ),
+                    'attr' => 'stripe',
+                    'type' => 'checkbox',
+					'value' => 'false',
+					'description' => __( 'Check this box to include stripes in the Progress Bar.', 'pbsandwich' ),
+                ),
+                array(
+                    'label' => __( 'Animated Progress Bar', 'pbsandwich' ),
+                    'attr' => 'animated',
+                    'type' => 'checkbox',
+					'value' => 'false',
+					'description' => __( 'Check this box to animate the Progress Bar.', 'pbsandwich' ),
+                ),					
+                array(
                     'label' => __( 'Current Percentage', 'pbsandwich' ),
                     'attr' => 'percentage',
                     'type' => 'text',
@@ -37,13 +71,13 @@ function sandwich_progressbar() {
                     'attr' => 'max-percentage',
                     'type' => 'text',
 					'value' => '100',
-                    'description' => __( 'Enter the initial percentage of the progress bar.', 'pbsandwich' ),
+                    'description' => __( 'Enter the maximum percentage of the progress bar.', 'pbsandwich' ),
                 ),
                 array(
                     'label' => __( 'Percentage suffix', 'pbsandwich' ),
                     'attr'  => 'suffix',
                     'type'  => 'text',
-					'value' => 'Completed',
+					'value' => '',
                     'description' => __( 'Enter text to go along beside the progress bar percentage.', 'pbsandwich' ),
                 ),
 			),
@@ -55,7 +89,12 @@ function sandwich_progressbar() {
 function sandwich_progressbar_shortcode( $attr, $content ) {
 		
 	$attr = wp_parse_args( $attr, array(
-        'title' => '',
+        'type' => 'progress-bar-info',
+        'stripe' => 'false',
+        'animated' => 'false',
+        'percentage' => '50',
+        'max-percentage' => '100',				
+        'suffix' => '',					
     ) );
 	
 	global $_sandwich_progressbar_id;
@@ -65,16 +104,23 @@ function sandwich_progressbar_shortcode( $attr, $content ) {
 	}
 	
 	$id = strtolower( str_replace( ' ', '-', preg_replace( '/[^a-zA-Z0-9 ]/', '', $attr['title'] ) ) ) . '-' . $_sandwich_progressbar_id++;
+
+	$additions = " " . esc_html( $attr['type'] );
+	if ( $attr['stripe'] == 'true' ) {
+		$additions .= ' progress-bar-striped';
+	}
+	if ( $attr['animated'] == 'true' ) {
+		$additions .= ' active';
+	}
 			
 	ob_start();
 	
 	?>
 	
 	<div class="sandwich">
-		<div class="panel panel-default toggle">
-			<div class="panel-body">
-				<a data-toggle="collapse" href="#<?php echo esc_attr( $id ) ?>" aria-expanded="false" aria-controls="<?php echo esc_attr( $id ) ?>"><?php echo esc_html( $attr['title'] ) ?></a>
-				<div class="collapse" id="<?php echo esc_attr( $id ) ?>"><?php echo wpautop( do_shortcode( $content ) ) ?></div>
+		<div class="progress">
+			<div id="progressbar-<?php echo esc_attr( $id ) ?>" class="progress-bar<?php echo $additions ?>" role="progressbar" aria-valuemin="0" aria-valuenow="<?php echo esc_html( $attr['percentage'] ) ?>" aria-valuemax="<?php echo esc_html( $attr['max-percentage'] ) ?>" style="width: <?php echo esc_html( $attr['percentage'] ) ?>%">
+				<span><?php echo esc_html( $attr['percentage'] ) . "% " . esc_html( $attr['suffix'] ); ?></span>
 			</div>
 		</div>
 	</div>
