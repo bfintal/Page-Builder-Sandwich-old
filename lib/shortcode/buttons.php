f2571b7bec660a073eea9b04324f9fd371cecbe9
Benjamin Intal <bfintal@gmail.com>
2015-03-13 14:46:24 +0800

    Text cleanup & code cleanup

--- lib/shortcode/buttons.php
+++ lib/shortcode/buttons.php
@@ -47,7 +47,7 @@ function sandwich_buttons_button_type() {
 add_action( 'init', 'sandwich_buttons', 11 );
 
 function sandwich_buttons() {
-	
+
 	add_shortcode( 'pbs_buttons', 'sandwich_buttons_shortcode' );
 
 	// Check if Shortcake exists
@@ -58,7 +58,7 @@ function sandwich_buttons() {
 	if ( ! is_admin() ) {
 		return;
 	}
-	
+
 	shortcode_ui_register_for_shortcode(
         'pbs_buttons',
         array(
@@ -66,110 +66,106 @@ function sandwich_buttons() {
             'listItemImage' => 'dashicons-plus',
             'attrs' => array(
                 array(
-                    'label' => __( 'Button Design', 'pbsandwich' ),
-                    'attr' => 'design',
-                    'type' => 'select',
-					'options' => sandwich_buttons_button_type(),
-					'description' => __( 'Choose the type of button to use. Ghost type makes the button translucent.', 'pbsandwich' ),
-                ),						
-                array(
                     'label' => __( 'Button Label', 'pbsandwich' ),
                     'attr' => 'caption',
                     'type' => 'text',
 					'value' => 'Click Here',
-					'description' => __( 'Enter the text to apply to the button.', 'pbsandwich' ),
                 ),
                 array(
-                    'label' => __( 'Button Color', 'pbsandwich' ),
+                    'label' => __( 'Button Design', 'pbsandwich' ),
+                    'attr' => 'design',
+                    'type' => 'select',
+					'options' => sandwich_buttons_button_type(),
+					'description' => __( 'Choose the design to use. Ghost type makes the button translucent with a colored border.', 'pbsandwich' ),
+                ),
+                array(
+                    'label' => __( 'Button Color Scheme', 'pbsandwich' ),
                     'attr' => 'bstyle',
                     'type' => 'select',
 					'options' => sandwich_buttons_style_selection(),
-					'description' => __( 'Choose the styling of buttons to use.', 'pbsandwich' ),
+					'description' => __( 'Choose the color scheme of the button.', 'pbsandwich' ),
                 ),
-                array(
-                    'label' => __( 'Button Text Color', 'pbsandwich' ),
-                    'attr' => 'textcolor',
-                    'type' => 'color',
-					'value' => '',
-					'description' => __( 'Choose the text color of the button.', 'pbsandwich' ),
-                ),				
                 array(
                     'label' => __( 'Custom Button Background / Border Color', 'pbsandwich' ),
                     'attr' => 'cbuttoncolor',
                     'type' => 'color',
-					'description' => __( 'Choose the background color of the button.', 'pbsandwich' ),
-                ),				
+					'description' => __( 'You can override the button color here. Leave this blank to use the default color scheme above.', 'pbsandwich' ),
+                ),
+                array(
+                    'label' => __( 'Text Color', 'pbsandwich' ),
+                    'attr' => 'textcolor',
+                    'type' => 'color',
+					'value' => '',
+					'description' => __( 'The color of the label of the button. Leave this blank to use the default color', 'pbsandwich' ),
+                ),
                 array(
                     'label' => __( 'Button Size', 'pbsandwich' ),
                     'attr' => 'size',
                     'type' => 'select',
 					'options' => sandwich_buttons_size_selection(),
-					'description' => __( 'Choose the size to render the button.', 'pbsandwich' ),
+					'description' => __( 'The size of the button.', 'pbsandwich' ),
                 ),
                 array(
-                    'label' => __( 'Custom Button Border', 'pbsandwich' ),
+                    'label' => __( 'Border Thickness', 'pbsandwich' ),
                     'attr' => 'cbuttonborder',
                     'type' => 'text',
 					'value' => '',
-					'description' => __( 'Enter a numerical value, in pixels, to define the thickness of the button border.', 'pbsandwich' ),					
+					'description' => __( 'Enter a number in pixels. Borders are only applicable for ghost buttons.', 'pbsandwich' ),
                 ),
                 array(
-                    'label' => __( 'Custom Button Border Radius', 'pbsandwich' ),
+                    'label' => __( 'Border Radius', 'pbsandwich' ),
                     'attr' => 'cbuttonradius',
                     'type' => 'text',
 					'value' => '',
-					'description' => __( 'Enter a numerical value, in pixels, to define the radius or roundness of the button.', 'pbsandwich' ),					
-                ),		
+					'description' => __( 'Enter a number in pixels.', 'pbsandwich' ),
+                ),
                 array(
-                    'label' => __( 'URL to link to', 'pbsandwich' ),
+                    'label' => __( 'Link URL', 'pbsandwich' ),
                     'attr' => 'url',
                     'type' => 'url',
 					'value' => '#',
-					'description' => __( 'To use the button as a link, enter the URL where the button should go to when clicked.', 'pbsandwich' ),
                 ),
                 array(
-                    'label' => __( 'Open link in new windows', 'pbsandwich' ),
+                    'label' => __( 'Open link in a new window?', 'pbsandwich' ),
                     'attr' => 'target',
                     'type' => 'checkbox',
 					'value' => 'true',
-					'description' => __( 'Check this box to open hyperlinks in a new window.', 'pbsandwich' ),
-                ),			
+                ),
                 array(
-                    'label' => __( 'Full-width Buttons', 'pbsandwich' ),
+                    'label' => __( 'Full-width Button', 'pbsandwich' ),
                     'attr' => 'blocklevel',
                     'type' => 'checkbox',
 					'value' => 'false',
-					'description' => __( 'Check this box to expand the buttons into full width.', 'pbsandwich' ),
-                ),				
+                ),
 			),
         )
     );
 }
 
 function sandwich_buttons_shortcode( $attr, $content ) {
-		
+
 	$attr = wp_parse_args( $attr, array(
-        'design' => 'normal',		
+        'design' => 'normal',
         'caption' => 'Click Here',
         'bstyle' => 'btn-default',
         'textcolor' => '',
         'cbuttoncolor' => '',
         'size' => 'btn-md',
         'cbuttonborder' => '',
-        'cbuttonradius' => '',		
+        'cbuttonradius' => '',
         'url' => '#',
-        'target' => 'true',		
-        'blocklevel' => 'false',		
+        'target' => 'true',
+        'blocklevel' => 'false',
     ) );
-	
+
 	global $_sandwich_buttons_id;
-	
+
 	if ( ! isset( $_sandwich_buttons_id ) ) {
 		$_sandwich_buttons_id = 1;
 	}
-	
+
 	$id = strtolower( str_replace( ' ', '-', preg_replace( '/[^a-zA-Z0-9 ]/', '', $attr['bstyle'] ) ) ) . '-' . $_sandwich_buttons_id++;
-	
+
 	$btype = "button";
 
 	$btnclass = " " . esc_attr( $attr['bstyle'] ) . " " . esc_attr( $attr['size'] );
@@ -199,24 +195,24 @@ function sandwich_buttons_shortcode( $attr, $content ) {
 	}
 	if ( $attr['cbuttonradius'] != '' ) {
 		$styling .= 'border-radius: ' . $attr['cbuttonradius'].'px; ';
-	}	
-	
+	}
+
 	$styling .= '"';
 
 	ob_start();
-	
+
 	?>
-	
+
 	<div class="sandwich">
 
-		<?php echo '<a id="button-' . esc_attr( $id ) . '" class="btn' . $btnclass . '"' . $appendices . $styling . '>'; 
+		<?php echo '<a id="button-' . esc_attr( $id ) . '" class="btn' . $btnclass . '"' . $appendices . $styling . '>';
 			echo esc_attr( $attr['caption'] );
-			echo '</a>';			
+			echo '</a>';
 		 ?>
 
 	</div>
-	
+
 	<?php
-		
+
 	return ob_get_clean();
 }
