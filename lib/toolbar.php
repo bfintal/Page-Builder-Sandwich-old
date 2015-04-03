ad64617f3cbc674ca6ef2136531954ba2409ede8
Benjamin Intal <bfintal@gmail.com>
2015-04-03 16:06:59 +0800

    Removed dummy toolbar label

--- lib/toolbar.php
+++ lib/toolbar.php
@@ -46,12 +46,6 @@ class GambitPBSandwichToolbar {
 			'label' => __( 'Clone', 'pbsandwich' ),
 			'priority' => 0,
 		);
-		$toolbarButtons[] = array(
-			// 'action' => 'clone',
-			'icon' => 'dashicons dashicons-images-alt',
-			'label' => __( 'Clone', 'pbsandwich' ),
-			'priority' => 0,
-		);
 		
 		// Allow others to add toolbar buttons
 		$toolbarButtons = apply_filters( 'pbs_toolbar_buttons', $toolbarButtons );
