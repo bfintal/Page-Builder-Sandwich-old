8834d7121dc14f300ed806fd242622fd6e56f97b
Herman <oherman@gmail.com>
2015-04-08 17:34:09 +0800

    Now supports the thumbnail type of embedding as well.

--- js/min/editor-min.js
+++ js/min/editor-min.js
@@ -6,6 +6,7 @@
 // @codekit-append "_editor-columns.js";
 // @codekit-append "_editor-column-actions.js";
 // @codekit-append "_editor-jetpack.js";
+// @codekit-append "_frontend-niconico-video.js";
 // @codekit-append "_editor-end.js";
 
 /**
@@ -1998,6 +1999,28 @@ editor.on('init', function() {
 		return false;
 	});
 });
+
+jQuery(document).ready(function($) {
+		var doc = $('pbs-nnd-video-thumb', window.parent.document ).contents();
+		var nur = doc.height('auto');
+		console.log(nur);
+});
+/**
+ * Click handler for the "Add Post Element" button. Basically we open the WP Media Manager then activate the shortcake state
+ */
+// jQuery(document).ready(function($) {
+// 	$('body').on('click', '.sandwich-add-shortcode', function() {
+// 		$(this).siblings('[id="insert-media-button"]').click();
+// 		$('.media-menu .media-menu-item:contains("' + shortcodeUIData.strings.media_frame_menu_insert_label + '")').click();
+// 		return false;
+// 	});
+// });
+//
+// $("#iframe-id").contents().find("img").attr("style","width:100%;height:100%")
+//
+// $("#iframe-id").contents().find("img").addClass("fancy-zoom")
+//
+// $("#iframe-id").contents().find("img").onclick(function(){ zoomit($(this)); });
 
 /**
  * This just closes the TinyMCE plugin call, appended last in editor.js
--- lib/shortcode/video-niconico.php
+++ lib/shortcode/video-niconico.php
@@ -7,6 +7,16 @@
 // Exit if accessed directly
 if ( ! defined( 'ABSPATH' ) ) exit;
 
+/**
+ * Embed type for Nico Nico Douga video.
+ */
+
+function sandwich_niconico_video_embed_type () {
+	$output['video'] = __( 'Video Player', 'pbsandwich' );
+	$output['thumb'] = __( 'Thumbnail and Description', 'pbsandwich' );
+	return $output;
+}
+
 
 /**
  * Create our shortcode
@@ -32,13 +42,26 @@ function sandwich_niconico_video() {
 		array(
 			'label' => __( 'Nico Nico Douga Video', 'pbsandwich' ),
 			'listItemImage' => 'dashicons-video-alt2',
-			'attrs' => array(			
+			'attrs' => array(
+				array(
+					'label' => __( 'Embed Type', 'pbsandwich' ),
+					'attr' => 'type',
+					'type' => 'select',
+					'description' => __( "Choose whether to embed the video itself or just a thumbnail and description of the video.", 'pbsandwich' ),
+					'options' => sandwich_niconico_video_embed_type (),
+				),
 				array(
 					'label' => __( 'Video URL', 'pbsandwich' ),
 					'attr' => 'videourl',
 					'type' => 'url',
 					'description' => __( 'The URL of the Nico Nico Douga Video. Video URLs look like this: http://www.nicovideo.jp/watch/smxxxxxxx.', 'pbsandwich' ),
 				),
+                array(
+                    'label' => __( 'Video Label', 'pbsandwich' ),
+                    'attr' => 'label',
+                    'type' => 'text',
+					'description' => __( 'You can put a description of your video here, that will only appear if JavaScript is unsupported. If none is given, the description defaults to the video URL.', 'pbsandwich' ),
+                ),				
 				// array(
 					// 'label' => __( 'Autoplay Video', 'pbsandwich' ),
 					// 'attr' => 'autoplay',
@@ -73,25 +96,43 @@ function sandwich_niconico_video() {
 function sandwich_niconico_video_shortcode( $attr, $content ) {
 		
 	$attr = wp_parse_args( $attr, array(
+		'type' => 'video',		
 		'videourl' => '',
+		'label' => '',		
 		'autoplay' => '',
 		'preload' => '',
 		'controls' => 'disabled',
 	) );
 
-	$uri_parts = parse_url( esc_attr( $attr['videourl'] ) );
+	$videourl = esc_attr( $attr['videourl'] );
+	$uri_parts = parse_url( $videourl );
 	$split_parts = explode('/', $uri_parts['path']);
 	
 	$args = $attr['autoplay'] == "true" && ! is_admin() ? ' autoplay="true"' : "";
 	$args .= $attr['preload'] == "auto" ? ' preload="auto"' : "";
 	$args .= $attr['controls'] == "disabled" ? ' controls' : "";
 	
+	if ( ! empty ( $attr['label'] ) ) {
+		$label = esc_attr( $attr['label'] );
+	} else {
+		$label = $videourl;
+	}
+
+	$label = 
+	
 	ob_start();
 	
 	?>
 	
-	<div class="sandwich niconicovideo">
-		<?php echo '<script type="text/javascript" charset="utf-8" src="http://ext.nicovideo.jp/thumb_watch/' . esc_attr( $split_parts[2] ) . '"></script>' ?>
+	<div class="sandwich pbs-niconicovideo">
+		<?php 
+		if ( $attr['type'] == 'thumb' ) {
+			echo '<iframe id="pbs-nnd-video-thumb" width="312" height="185" scrolling="no" frameborder="0" src="http://ext.nicovideo.jp/thumb/' . esc_attr( $split_parts[2] ) . '"><a href="' . esc_attr( $attr['videourl'] ) . '" target="_blank">' . $label . '</a></iframe>';			
+		}
+		else {
+			echo '<script type="text/javascript" charset="utf-8" src="http://ext.nicovideo.jp/thumb_watch/' . esc_attr( $split_parts[2] ) . '" scrolling="no" style="border:solid 1px #CCC;" frameborder="0"></script><noscript><a href="' . esc_attr( $attr['videourl'] ) . '" target="_blank">' . $label . '</a></noscript>';
+		}
+		?>
 	</div>
 	
 	<?php
